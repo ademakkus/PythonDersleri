7f932cb2e0e11a8c5efa01c13a2b7d256b7c785f
muratvuranok <mrtvrnk@gmail.com>
2019-06-23 12:05:00 +0300

    karar yapıları

--- Lesson3/KararYapilari5.py
+++ Lesson3/KararYapilari5.py
@@ -1,15 +1,29 @@
+# Mantıksal operatoler
+
+# and  sorguya katılan her bir koşulun sağlanması durumu
+# or   sorguya katılan hergangi bir koşulun sağlanması durumu
+# not  sorguya olumsuzluk katar True ise False, False ise True
+
+
+
 
 
 
 
 
 user_name = input("Lütfen kullanıcı adınızı giriniz : ")
-password  = input("Lütfen şifrenizi giriniz : ")
-
-if user_name == "admin":
+if user_name == "admin":  # db içerisinde varmı ?
+    password = input("Lütfen şifrenizi giriniz : ")
     if password == "123":
-        pass
+        print("Giriş yaptınız!")
     else:
-        pass
+        print("Şifreniz yanlış")
 else:
+    print("Kullanıcı adınız yanlış")
     pass
+
+
+if user_name == "admin" and password == "123":
+    print( "Tebrikler!")
+else:
+    print("Kullanıcı bilgilerinizi kontrol ediniz")
