3c334a3ff45fddef59a80a9f952c0324c81bd613
muratvuranok <mrtvrnk@gmail.com>
2019-06-23 11:02:39 +0300

    karar yapıları

--- Lesson3/KararYapilari2.py
+++ Lesson3/KararYapilari2.py
@@ -1,1 +1,26 @@
 # Örnek : Girilen not değeri 0’dan küçük ise, 0’dan küçük not giremezsiniz. 100’den büyük bir değer girerseniz 100’den büyük not girişi yapamazsınız uyarı verdiren uygulama yazınız.
+
+
+
+try:
+    not_ = int(input("Lütfen notunuzu giriniz : "))
+    if not_ < 0:
+        print("0'dan küçük not girişi yapamazsınız!")
+    elif not_ > 100:
+        print("100'den büyük not girişi yapamazsınız!")
+    else:
+        print("Notunuz :",not_)
+
+except ValueError as exp:
+    print("Lütfen sayısal veri giriniz.")
+except Exception as exp:
+    print("Belirlenemeyen bir hata çeşidi.")
+
+
+
+
+
+
+
+
+
