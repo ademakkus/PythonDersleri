cb081873a67858009cd3b6af704f374af4b02dcb
muratvuranok <mrtvrnk@gmail.com>
2019-06-23 10:58:15 +0300

    karar yapıları

---
+++ Lesson3/KararYapilari1.py
@@ -0,0 +1,64 @@
+# karar yapıları if - elif - else
+
+# Karılaştırma operatorleri
+
+# == (Eşittir) Soldaki değerin, sağdaki değere eşit olma durumu
+# Örnek : 1 == 1 Sonuç => True
+#         4 == 2 Sonuc => False
+
+# != (Eşit Değildir) Soldaki değerin, sağdaki değere eşit olmama durumu
+# Örnek : 1 != 2 Sonuc => True
+#         1 != 1 Sonuc => False
+
+
+# >   (Büyüktür) Soldaki değerin, sağdaki değerden büyük olma durumu
+# Örnek : 3 > 1 Sonuc => True
+#         1 > 2 Sonuc => False
+
+
+# <    (Küçüktür) Soldaki değerin, sağdaki değerden küçük olma durumu
+# Örnek : 1 < 2 Sonuc => True
+#         4 < 1 Sonuc => False
+
+
+
+# >=  (Büyük veya Eşit) Soldaki değerin, sağdaki değerden büyük veya eşit olma durumu
+# Örnek : 1 >= 1 Sonuc => True (eşitlik)
+#         3 >= 2 Sonuc => True (büyüklük)
+#         1 >= 2 Sonuc => False
+
+
+# <=  (Küçük veya Eşit) Soldaki değerin, sağdaki değerden küçük veya eşit olma durumu
+
+# Örnek : 1 <= 1 Sonuc => True (eşitlik)
+#         1 <= 3 Sonuc => True (küçüklük)
+#         4 <= 1 Sonuc => False
+
+
+
+
+username = input("Lütfen kullanıcı adınızı giriniz : ")
+username = username\
+    .lower()\
+    .replace("ı","i")\
+    .replace('̇n','n')\
+    .replace("ç","c")\
+    .replace("ş","s")\
+    .replace("ö","o")\
+    .replace("ğ","g")
+
+print(username)
+if(username == "admin"):
+    print("Giriş yaptınız!")
+else:
+    print("Kullanıcı adınız hatalı!")
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson3/KararYapilari2.py
@@ -0,0 +1,1 @@
+# Örnek : Girilen not değeri 0’dan küçük ise, 0’dan küçük not giremezsiniz. 100’den büyük bir değer girerseniz 100’den büyük not girişi yapamazsınız uyarı verdiren uygulama yazınız.
